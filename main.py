535246a8325b77045c2823d001cde2e1d7cb3c49
alexciru <alejandro.cirugeda@estudiante.uam.es>
2019-05-05 17:18:34 +0200

    add files

---
+++ main.py
@@ -0,0 +1,16 @@
+# File: main.py
+# Author: Alejandro Cirugeda
+# Description:
+#
+#
+
+
+def main():
+    # read file
+    # create points in the cluster
+    # start clustering
+    # show results
+    return
+
+if __name__ == "__main__":
+    main()
---
+++ point.py
@@ -0,0 +1,13 @@
+# File: point.py
+# Author: Alejandro Cirugeda
+# Description:
+#   The class Point will storage the information about the different transactions from the file read
+#
+
+
+class Point:
+
+    __init__(Self, x, y):
+        self.x = x
+        self.y = y
+        return
